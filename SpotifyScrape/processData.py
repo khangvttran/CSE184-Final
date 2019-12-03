bc8355fb3a8b1c02f1b86bc68a747e000adb59ed
Gus Person <gperson@ucsc.edu>
2019-12-03 15:31:30 -0800

    added edge cases

--- SpotifyScrape/processData.py
+++ SpotifyScrape/processData.py
@@ -10,7 +10,7 @@ client_secret = "81fa55c01dd94bd1b8129c602f8eefb6"
 client_credentials_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
 #spotipy object to get spotify data
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) 
-columns = ['Artist', 'Song', 'Duration(ms)', 'Year', 'danceability', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+columns = ['Artist', 'Song', 'Duration(ms)', 'Year', 'danceability', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'uri']
 df = pd.DataFrame(columns=columns)
 top100 = pd.read_csv('2005-2018top100.csv')
 number = 0
@@ -22,6 +22,24 @@ for index, row in top100.iterrows():
 	if 'Featuring' in artist:
 		artistSpl = artist.split('Featuring')
 		artist = artistSpl[0]
+	# if '(' in artist:
+	# 	artistSpl = artist.split('(')
+	# 	artist = artistSpl[0]
+	if '&' in artist:
+		artistSpl = artist.split('&')
+		artist = artistSpl[0]
+	if '$' in artist:
+		artist = artist.replace('$', 's')
+	if '\'' in artist:
+		artist = artist.replace('\'', '')
+
+
+	if '(' in song:
+		songspl = song.split('(')
+		song = songspl[0]
+	if '\'' in song:
+		song = song.replace('\'', '')
+
 	try:
 		results = sp.search(q='artist:' + artist + ' track:' + song, type='track')
 		id = results['tracks']['items'][0]['id']
@@ -37,12 +55,14 @@ for index, row in top100.iterrows():
 		valence = audiofeat[0]['valence']
 		tempo = audiofeat[0]['tempo']
 		key = audiofeat[0]['key']
+		uri = audiofeat[0]['uri']
 		# data = {'Rank': [rank], 'Album': [album], 'Artist': [artist], 'Year': [year]}
-		data = {'Artist': [artist], 'Song': [song], 'Duration(ms)' : [duration], 'Year': [year], 'danceability' : [danceability], 'energy' : [energy], 'key': [key], 'loudness': [loudness], 'speechiness': [speechiness], 'acousticness': [acousticness], 'instrumentalness': [instrumentalness], 'liveness': [liveness], 'valence': [valence], 'tempo': [tempo] }
+		data = {'Artist': [artist], 'Song': [song], 'Duration(ms)' : [duration], 'Year': [year], 'danceability' : [danceability], 'energy' : [energy], 'key': [key], 'loudness': [loudness], 'speechiness': [speechiness], 'acousticness': [acousticness], 'instrumentalness': [instrumentalness], 'liveness': [liveness], 'valence': [valence], 'tempo': [tempo], 'uri': [uri] }
 		dfRow = pd.DataFrame(data, columns=columns)
 		df = pd.concat([df, dfRow], ignore_index=True)
+		print(song + ' by ' + artist + ' added!')
 	except:
-		print(song + 'Not found!')
+		print(song + ' by ' + artist + 'Not found!')
 		continue
 
 	# try:
@@ -73,4 +93,4 @@ for index, row in top100.iterrows():
 	# if number == 25:
 	# 	break
 print(df)
-df.to_csv('songDataTop100ByYear.csv')
+df.to_csv('songDataTop100ByYearfixtest.csv')
